81256efc62d71a8a64a62940d210ba968c6fc3c6
Derrick <derrickrlewis@gmail.com>
2020-10-28 21:54:40 -0500

    Added buttons

--- app/src/main/java/io/figure8/deadpresidents/MainActivity.java
+++ app/src/main/java/io/figure8/deadpresidents/MainActivity.java
@@ -2,13 +2,28 @@ package io.figure8.deadpresidents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_addOne;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_addOne = findViewById(R.id.btn_addOne);
+
+        btn_addOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,AddEditOne.class);
+                startActivity(intent);
+            }
+        });
     }
 }
