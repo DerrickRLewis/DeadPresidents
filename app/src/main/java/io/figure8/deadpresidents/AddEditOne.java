61d02725c5ddcb97af5506ca4af761029f8a5ab5
Derrick <derrickrlewis@gmail.com>
2020-10-30 02:30:36 -0500

    Added buttons

--- app/src/main/java/io/figure8/deadpresidents/AddEditOne.java
+++ app/src/main/java/io/figure8/deadpresidents/AddEditOne.java
@@ -2,13 +2,42 @@ package io.figure8.deadpresidents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddEditOne extends AppCompatActivity {
+
+    Button btn_ok, btn_cancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_edit_one);
+
+        btn_ok = findViewById(R.id.btn_Ok);
+        btn_cancel = findViewById(R.id.btn_Cancel);
+
+        btn_ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AddEditOne.this,AddEditOne.class);
+                startActivity(intent);
+            }
+        });
+        btn_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AddEditOne.this,AddEditOne.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
+
     }
 }
